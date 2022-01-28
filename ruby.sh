d2f492ea11677b4b785fd8488993c4c9082ceca7
voanhq <voanhq@gmail.com>
2022-01-28 10:47:09 +0700

    update ruby installation script

--- ruby.sh
+++ ruby.sh
@@ -1,24 +1,22 @@
 
 # https://www.digitalocean.com/community/tutorials/how-to-install-ruby-on-rails-with-rbenv-on-ubuntu-20-04
-
 cd ~
 sudo apt update
-sudo apt install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
+sudo apt install -y git curl autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
 
-# RBENV and RUBY-BUILD
+# RBENV & RUBY-BUILD
+rm -rf .rbenv/
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+echo "" >> ~/.bashrc
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
-git clone https://github.com/rbenv/ruby-build.git
-PREFIX=/usr/local sudo ./ruby-build/install.sh
 
-# RUBY
-rbenv install 3.1.0
-rbenv global 3.1.0
+# # # # RUBY
+rbenv install 3.0.2
+rbenv global 3.0.2
 ruby -v
 
-# RAILS
 echo "gem: --no-document" > ~/.gemrc
 gem install bundler
 gem install rails
