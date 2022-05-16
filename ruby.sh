f87c96020aa409c56150d269952fd8f41dc66f63
voanhq <voanhq@gmail.com>
2022-05-17 01:30:50 +0700

    ruby

--- ruby.sh
+++ ruby.sh
@@ -1,5 +1,6 @@
 
 # https://www.digitalocean.com/community/tutorials/how-to-install-ruby-on-rails-with-rbenv-on-ubuntu-20-04
+# https://askubuntu.com/questions/1399788/ruby-installation-build-failed-ubuntu-20-04-using-ruby-build-20220324
 cd ~
 sudo apt update
 sudo apt install -y git curl autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev libpq-dev
