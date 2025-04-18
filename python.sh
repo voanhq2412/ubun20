9057944f58d1732f2aae192854d790d776a3bb27
voanhq <voanhq@gmail.com>
2025-04-19 03:20:45 +0700

    create requirements.txt for python

--- python.sh
+++ python.sh
@@ -5,30 +5,12 @@ uv python install 3.7 3.12
 uv venv ~/.virtualenvs/py37 --python 3.7
 uv venv ~/.virtualenvs/py312 --python 3.12
 
+SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+echo "Script is located in: $SCRIPT_DIR"
+
 source ~/.virtualenvs/py37/bin/activate
-uv pip install numpy
-uv pip install scikit-learn
-uv pip install tensorflow
-uv pip install opencv-python
-uv pip install pandas
-uv pip install selenium
-uv pip install jupyterlab
-uv pip install plotly
-uv pip install matplotlib
-uv pip install black isort
-uv pip install numba
-uv pip install shapely
+uv pip install -r "$SCRIPT_DIR/requirements.txt"
 
 source ~/.virtualenvs/py312/bin/activate
-uv pip install numpy
-uv pip install scikit-learn
-uv pip install tensorflow
-uv pip install opencv-python
-uv pip install pandas
-uv pip install selenium
-uv pip install jupyterlab
-uv pip install plotly
-uv pip install matplotlib
-uv pip install black isort
-uv pip install numba
-uv pip install shapely
+uv pip install -r "$SCRIPT_DIR/requirements.txt"
